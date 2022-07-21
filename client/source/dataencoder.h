bd855e4c4d14d7105335f1f5381fe285fb349f0e
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 21:48:40 +0800

    Update dataencoder.h

--- client/source/dataencoder.h
+++ client/source/dataencoder.h
@@ -8,14 +8,14 @@ using namespace std;
 class DataEncoder
 {
 private:
-    char head[BASE_BUFFER_SIZE];
-    char * hp;
-    void encodeElement(unsigned int data, unsigned int len);
+    char head[BASE_BUFFER_SIZE];  // 定义一个字符串 缓存大小
+    char * hp;  // 定义一个字符串指针
+    void encodeElement(unsigned int data, unsigned int len);  // 定义一个编码元素的函数 它包括数据和长度两个参数
 
 public:
     DataEncoder();
     string encode(unsigned int protocolId, unsigned int account, unsigned int dataType, unsigned int dataLength);
-
+    // 定义一个字符串 它包含这些参数：协议ID 账号 数据类型 数据长度
 };
 
 
