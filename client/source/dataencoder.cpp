10e2c5f3b9a5f23f9a1fa26dd9d43ec960d1d0b7
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 21:54:16 +0800

    Update dataencoder.cpp

--- client/source/dataencoder.cpp
+++ client/source/dataencoder.cpp
@@ -6,14 +6,14 @@ DataEncoder::DataEncoder() {
 
 string DataEncoder::encode(unsigned int protocolId, unsigned int account, unsigned int dataType, unsigned int dataLength) {
     hp = head;
-    encodeElement(protocolId, PROTOCOL_ID_SIZE);
-    encodeElement(account, ACCOUNT_SIZE);
-    encodeElement(dataType, DATA_TYPE_SIZE);
-    encodeElement(dataLength, DATA_SIZE);
-    return string(head,sizeof(head));
+    encodeElement(protocolId, PROTOCOL_ID_SIZE);  // 协议ID 协议ID大小
+    encodeElement(account, ACCOUNT_SIZE);  // 账号 账号大小
+    encodeElement(dataType, DATA_TYPE_SIZE);  // 数据类型 数据类型大小
+    encodeElement(dataLength, DATA_SIZE);  // 数据长度 数据大小
+    return string(head,sizeof(head));  // 返回 头和头大小 的字符串
 }
 
-void DataEncoder::encodeElement(unsigned int data, unsigned int len) {
+void DataEncoder::encodeElement(unsigned int data, unsigned int len) {  // 对数据和长度做一个编码
     char* c = hp + len - 1;
     for (int i = len; i > 0; i--) {
         *c = (char)(data & 0xff);
