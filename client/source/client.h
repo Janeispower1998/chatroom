5909b11794b9ec3bf77effa5a640993fde19a2a0
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 16:54:28 +0800

    Update client.h

--- client/source/client.h
+++ client/source/client.h
@@ -15,36 +15,36 @@
 #include"dataencoder.h"
 #include"configreader.h"
 #define IMAGE_PATH "./image/"
-#define TCP_BUFSIZ 8192
+#define TCP_BUFSIZ 8192  // TCP缓存大小是8192
 using namespace std;
 
 class Client
 {
 public:
-    QString hostName;
-    quint16 port;
-    QTcpSocket tcpClient;
-    char buffer[TCP_BUFSIZ];
-    unsigned int protocolId;
-    unsigned int account;
-    unsigned int dataType;
-    unsigned int dataLength;
+    QString hostName;  // host
+    quint16 port;  // port 16为无符号整型
+    QTcpSocket tcpClient;  // socket
+    char buffer[TCP_BUFSIZ];  // 缓存
+    unsigned int protocolId;  // 协议ID
+    unsigned int account;  // 账号
+    unsigned int dataType;  // 数据类型
+    unsigned int dataLength;  // 数据长度
     const string ACK_PACKET = DataEncoder().encode(ACK,0,0,0);
 
 public:
     Client();
 
-    void readHeadData();
+    void readHeadData();  // 定义一个读取头信息的函数
 
-    QJsonObject readServerMsg();
+    QJsonObject readServerMsg();  // 封装一个JSON对象
 
-    void writeText(unsigned int account ,string text, unsigned int protocolId = SEND);
+    void writeText(unsigned int account ,string text, unsigned int protocolId = SEND);  // 封装一个写文字的函数
 
-    void closeTcpSocket();
+    void closeTcpSocket();  // 封装一个关掉TCP socket的函数
 
-    QTcpSocket * getTcpClient();
+    QTcpSocket * getTcpClient();  // 
 
-    ~Client();
+    ~Client();  // 析构
 };
 
 #endif // CLIENT_H
