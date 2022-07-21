64f54850076f3f6b41ccee27a021fd153cd2e1ca
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 17:28:09 +0800

    Update configreader.h

--- client/source/configreader.h
+++ client/source/configreader.h
@@ -7,10 +7,10 @@ using namespace std;
 class ConfigReader
 {
 private:
-    static string SERVER_CONFIG_PATH;
+    static string SERVER_CONFIG_PATH;  // 字符串静态变量 服务器装置的路径 
 public:
-    ConfigReader();
-    static unordered_map<string, string> readServerConfig();
+    ConfigReader();  // 封装这个类
+    static unordered_map<string, string> readServerConfig();  // 封装一个读取服务器装置的函数
 };
 
 #endif // CONFIGREADER_H
