878f26497d7437538f6e2d4513b497ed183699c4
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 22:12:45 +0800

    Update dataparser.h

--- client/source/dataparser.h
+++ client/source/dataparser.h
@@ -6,27 +6,27 @@ class DataParser
     //协议号(1B)-账号(2B)-数据类型(1B)-数据长度(4B)-数据
 private:
 
-    char* bp;
-    unsigned int protocolId;
-    unsigned int account;
-    unsigned int dataType;
-    unsigned int dataLength;
+    char* bp;  // 定义一个字符串指针bp
+    unsigned int protocolId;  // 定义一个整型 协议ID
+    unsigned int account;  // 账号
+    unsigned int dataType;  // 数据类型
+    unsigned int dataLength;  // 数据长度
 
-    unsigned int parseInt(int len);
+    unsigned int parseInt(int len);  // 解析整型
 
 public:
 
     DataParser(char * buffer);
 
-    bool baseParse();
+    bool baseParse();  
 
-    unsigned int getProtocolId();
+    unsigned int getProtocolId();  // 定义一个函数 获取协议ID
 
-    unsigned int getAccount();
+    unsigned int getAccount();  // 定义一个函数 获取账号
 
-    unsigned int getDataType();
+    unsigned int getDataType();  // 定义一个函数 获取数据类型
 
-    unsigned int getDataLength();
+    unsigned int getDataLength();  // 定义一个函数 获取数据长度
 
     ~DataParser();
 };
