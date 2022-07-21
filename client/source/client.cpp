38a1d1af32f33ec652ef1209fa145235bfdae66a
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 17:25:01 +0800

    Update client.cpp

--- client/source/client.cpp
+++ client/source/client.cpp
@@ -6,41 +6,41 @@ Client::Client()
 }
 
 QJsonObject Client::readServerMsg(){
-    readHeadData();
-    tcpClient.read(buffer, dataLength);
-    QString serverMsg = QString::fromStdString(string(buffer, dataLength));
-    QJsonObject serverInfoObj = QJsonDocument::fromJson(serverMsg.toUtf8()).object();
-    return serverInfoObj;
+    readHeadData();  // 读取头部信息
+    tcpClient.read(buffer, dataLength);  // 读取缓存和数据长度
+    QString serverMsg = QString::fromStdString(string(buffer, dataLength));  // 封装string对象
+    QJsonObject serverInfoObj = QJsonDocument::fromJson(serverMsg.toUtf8()).object();   // 封装JSON对象
+    return serverInfoObj;  // 返回JSON对象的信息
 }
 
 void Client:: readHeadData(){
-    int size = tcpClient.read(buffer, BASE_BUFFER_SIZE);
-    DataParser parser(buffer);
-    parser.baseParse();
-    protocolId = parser.getProtocolId();
-    account = parser.getAccount();
-    dataType = parser.getDataType();
-    dataLength = parser.getDataLength();
+    int size = tcpClient.read(buffer, BASE_BUFFER_SIZE);  // 读取缓存和基本缓存大小
+    DataParser parser(buffer);  // 数据解析器解析buffer
+    parser.baseParse();  // 解析
+    protocolId = parser.getProtocolId();  // 解析协议ID
+    account = parser.getAccount();  // 解析账户
+    dataType = parser.getDataType();  // 解析数据类型
+    dataLength = parser.getDataLength();  // 解析数据长度
 }
 
-void Client:: writeText(unsigned int account ,string text, unsigned int protocolId){
-    DataEncoder encoder;
+void Client:: writeText(unsigned int account ,string text, unsigned int protocolId){  // 写文字
+    DataEncoder encoder;  // 封装一个编码器
     qDebug() << "------------输入的字节数-------------" << text.length();
-    string headStr = encoder.encode(protocolId, account, TEXT, text.length());
-    tcpClient.write(headStr.data(), headStr.length());
+    string headStr = encoder.encode(protocolId, account, TEXT, text.length());  // 用字符串来表示协议ID，账号，文字和文字长度的编码
+    tcpClient.write(headStr.data(), headStr.length());  // 写出以上字符串的数据和长度
     qDebug() << "------------头部大小-------------" << headStr.length();
     //text = headStr + text;
-    if(text.length() != 0){
-        tcpClient.write(text.data(), text.length());
+    if(text.length() != 0){  // 如果文字长度不为零
+        tcpClient.write(text.data(), text.length());  // 那就写出数据和长度
     }
     qDebug() << "------------发送成功,总数据大小-------------" << text.length();
 }
 
 QTcpSocket * Client::getTcpClient(){
-    return &tcpClient;
+    return &tcpClient;  // 返回tcpClient的地址
 }
 
-void Client:: closeTcpSocket(){
+void Client:: closeTcpSocket(){  // 关掉TCP socket
     tcpClient.disconnectFromHost();
     tcpClient.close();
 }
