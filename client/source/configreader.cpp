5460151db93559a043980b48b15048231e8b1736
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-21 21:35:51 +0800

    Update configreader.cpp

--- client/source/configreader.cpp
+++ client/source/configreader.cpp
@@ -5,14 +5,14 @@ ConfigReader::ConfigReader()
 }
 unordered_map<string, string> ConfigReader::readServerConfig(){
     ifstream in(SERVER_CONFIG_PATH, ios::in);
-    unordered_map<string, string> configMap;
-    if(in.is_open()){
-        string line;
+    unordered_map<string, string> configMap;  // 创建一个无序的map 装置map
+    if(in.is_open()){  // 如果in打开了
+        string line;  // 定义一个字符串类型
         while(getline(in, line)){
-            int index = line.find('=');
-            configMap[line.substr(0, index)] = line.substr(index+1);
+            int index = line.find('=');  // 寻找这个字符串里面的等于号
+            configMap[line.substr(0, index)] = line.substr(index+1);  // configMap里面放置等号后面的字符串
         }
     }
-    return configMap;
+    return configMap;  // 返回configMap
 }
-string ConfigReader::SERVER_CONFIG_PATH = "./config/server.conf";
+string ConfigReader::SERVER_CONFIG_PATH = "./config/server.conf";  // 指定服务器配置的路径
