30fc031fa0d89848418f39870ed8e0bf20688906
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-29 14:30:37 +0800

    Update HeadData.h

--- server/ProtocolHead/HeadData.h
+++ server/ProtocolHead/HeadData.h
@@ -13,8 +13,8 @@ using namespace std;
 class HeadData {
     //协议号(1B)-账号(2B)-数据类型(1B)-数据长度(4B)-数据
 private:
-    char buffer[BASE_BUFFER_SIZE];
-    const char *bp;
+    char buffer[BASE_BUFFER_SIZE];  // 8位缓存
+    const char *bp;  // bp是后面用来指针运算的
     unsigned int protocolId;
     unsigned int account;
     unsigned int dataType;
