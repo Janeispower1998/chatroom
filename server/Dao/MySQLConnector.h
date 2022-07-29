2dbb71bc05b107af090a857f28cd8a6c367cb8d2
Janeispower1998 <78691315+Janeispower1998@users.noreply.github.com>
2022-07-29 11:00:09 +0800

    Update MySQLConnector.h

--- server/Dao/MySQLConnector.h
+++ server/Dao/MySQLConnector.h
@@ -11,14 +11,15 @@
 #include "../config/mysql_config.h"
 using namespace std;
 
+// 声明一个连接器
 class MySQLConnector {
-private:
-    MYSQL *conn;
-    static MySQLConnector *connector;
-    void init();
+private:  // 只能在这个类的内部用private里面的东西
+    MYSQL *conn;  // MYSQL是<mysql/mysql.h>中的一个类，声明了这个类的一个指针conn
+    static MySQLConnector *connector;  // 声明这个连接器自己的指针 它是静态的 就是不管生成多少个连接器对象 那些连接器对象都与这个是关联的
+    void init();  // 初始化
 
-public:
-    static MySQLConnector *getMySQLConnector();
+public:  
+    static MySQLConnector *getMySQLConnector();  // 找到连接器 服务器需要找到MySQL连接器的一个东西
 
     pair<int, string> queryUser(const string& account, const string& password);
 
